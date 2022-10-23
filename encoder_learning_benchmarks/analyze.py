e8c7a12564c047442afaa756cd47278d70aaffc3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 03:31:24 +0000

    Adding tarfile member sanitization to extractall()

--- encoder_learning_benchmarks/analyze.py
+++ encoder_learning_benchmarks/analyze.py
@@ -235,7 +235,26 @@ def load_benchmark_data(tar):
         with tempfile.TemporaryDirectory() as d, tarfile.open(tar) as ar:
             sys.stderr.write("Extracting archive...")
             sys.stderr.flush()
-            ar.extractall(d)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(ar, d)
             return _load_benchmark_data_dir(d)
     raise RuntimeError("Target is neither a directory nor a tar archive")
 
